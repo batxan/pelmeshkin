877e81aeb34d460121c28a8998a0ed62295070e5
batxan <batxan@mail.ru>
2026-08-13 11:18:56 +0300

    Update products.js

--- products.js
+++ products.js
@@ -53,22 +53,6 @@ window.PRODUCTS = [
   },
 
 
-  
- {
-    id: 4,
-    name: "Вареники с картошкой, 1 кг",
-    price: 850,
-    category: "Вареники2",
-    weight: "1 кг",
-    image: "https://images.unsplash.com/photo-1541014741259-de529411b96a?auto=format&fit=crop&w=600&q=80",
-    gallery: [
-      "https://images.unsplash.com/photo-1541014741259-de529411b96a?auto=format&fit=crop&w=600&q=80"
-    ],
-    description: "Вареники с нежной картофельной начинкой.",
-    ingredients: "Картофель, лук, тесто, соль, специи.",
-    storage: "Хранить в морозильной камере при -18°C."
-  }
-
 
 
 
